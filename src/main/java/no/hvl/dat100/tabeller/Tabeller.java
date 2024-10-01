0d44ade24d19e264a670d1921446e17713c3431a
Vitor-dot98 <673592@stud.hvl.no>
2024-10-01 17:40:30 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -4,28 +4,46 @@ public class Tabeller {
 
 	// a)
 	public static void main(String[]args) {
-	int tabell [] = {2,4,6,8,10};
+	int tabell [] = {1,2,3,4,5,6};
     skrivUt(tabell);
-	}
+    
+    int tabell2[] = {42,67,89};
+	
+	System.out.println(tilStreng(tabell2));
+    }
 
 	public static void skrivUt(int[] tabell) {
 		for (int i = 0; i < tabell.length; i++) {
 			System.out.println(tabell[i]);
+			System.out.println();
 		}
-		return;
+		
 	}
-
-
-	
 
 
 	// b)
-	public static String tilStreng(int[] tabell) {
+	
+	
+	
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+	public static String tilStreng(int[] tabell2) {
+	
+		StringBuilder sb = new StringBuilder();
+			sb.append("[");
+			
+			for (int i = 0; i < tabell2.length; i++) {
+			sb.append(tabell2[i]);
+			
+			
+			if(i < tabell2.length - 1) {
+			sb.append(",");	
+			}
+			}
+			sb.append("]");
+			return sb.toString();
+			
+	
 	}
-
 	// c)
 	public static int summer(int[] tabell) {
 
